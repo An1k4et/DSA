e4fcb2fa2f2429304d4dd61679cf53a5bf5cfd90
Aniket Vijay Vishwakarma <114073266+An1k4et@users.noreply.github.com>
2025-10-26 23:31:03 +0530

    Time: 112 ms (8.8%), Space: 46.8 MB (50.3%) - LeetHub

--- 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.java
+++ 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.java
@@ -1,52 +1,49 @@
 class Solution {
-    private void reverseRotate(StringBuilder sb, int left, int right){
-        while(left < right){
-            char ch = sb.charAt(left);
-            sb.setCharAt(left, sb.charAt(right));
-            sb.setCharAt(right, ch);
-            left++;
-            right--;
-        }
-    }
     private String rotate(String s, int b){
         StringBuilder sb = new StringBuilder(s);
         sb.reverse();
-        reverseRotate(sb, 0, b-1);
-        reverseRotate(sb, b, sb.length()-1);
-
+        reverse(sb, 0, b-1);
+        reverse(sb, b, sb.length()-1);
         return sb.toString();
     }
+    private void reverse(StringBuilder sb, int l, int r){
+        while(l < r){
+            char ch = sb.charAt(l);
+            sb.setCharAt(l, sb.charAt(r));
+            sb.setCharAt(r, ch);
+            l++;
+            r--;
+        }
+    }
     public String findLexSmallestString(String s, int a, int b) {
-        Set<String> visited = new HashSet<>();
         Queue<String> q = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
 
         q.add(s);
-        visited.add(s);
-        String smallestStr = s;
+        String smallString = s;
         while(!q.isEmpty()){
             String curr = q.poll();
 
-            if(curr.compareTo(smallestStr) < 0){
-                smallestStr = curr;
+            if(curr.compareTo(smallString) < 0){
+                smallString = curr;
             }
-
             char alpha[] = curr.toCharArray();
-            //Add a to each character
-            for(int i=1;i<alpha.length;i+=2){
-                int newDigit = ((alpha[i] - '0') + a) % 10;
-                alpha[i] = (char) (newDigit + '0');
+            for(int i=0;i<alpha.length;i++){
+                if(i%2 != 0){
+                    int newVal = ((alpha[i] - '0') + a) % 10;
+                    char ch = (char)('0' + newVal);
+                    alpha[i] = ch;
+                }
             }
-            String org = new String(alpha);
-            if(visited.add(org)){
-                q.add(org);
+            String addCurr = new String(alpha);
+            if(visited.add(addCurr)){
+                q.add(addCurr);
             }
-            //Rotate String by b 
-            String rotateString = rotate(curr, b);
-            if(visited.add(rotateString)){
-                q.add(rotateString);
+            String rotateCurr = rotate(curr, b);
+            if(visited.add(rotateCurr)){
+                q.add(rotateCurr);
             }
         }
-
-        return smallestStr;
+        return smallString;
     }
 }
