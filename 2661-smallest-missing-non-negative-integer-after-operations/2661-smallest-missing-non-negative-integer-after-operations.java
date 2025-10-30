f141ed57c789c875a3dd526d8110eec63d5718d5
Aniket Vijay Vishwakarma <114073266+An1k4et@users.noreply.github.com>
2025-10-30 23:57:41 +0530

    Time: 73 ms (7.87%), Space: 63.5 MB (20.57%) - LeetHub

--- 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.java
+++ 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.java
@@ -1,17 +1,15 @@
 class Solution {
-    public int findSmallestInteger(int[] nums, int values) {
+    public int findSmallestInteger(int[] nums, int value) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(int num: nums){
-            int temp = ((num % values) + values) % values;
-            map.put(temp , map.getOrDefault(temp, 0) + 1);
+        for(int num : nums){
+            int temp = ((num % value) + value) % value;
+            map.put(temp, map.getOrDefault(temp, 0) + 1);
         }
         int MEX = 0;
-
-        while(map.getOrDefault(MEX % values, 0) > 0){
-            map.put(MEX % values , map.get(MEX % values) - 1);
+        while(map.getOrDefault(MEX % value, 0) > 0){
+            map.put(MEX % value, map.getOrDefault(MEX % value, 0) - 1);
             MEX++;
         }
-
         return MEX;
     }
 }
